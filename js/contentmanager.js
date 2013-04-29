294b60fe46180f6db85f6386a1728dd29de198a2
Ripul Thakur <ripul.thakur@accenture.com>
2013-04-29 16:29:44 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -794,13 +794,6 @@ function categSel()
 {
 //put the selected category to further use
 selected_cat=document.getElementById('cat_sel').value;
-if (selected_cat=='val1')
-{
-$("#cat_select_items_button").hide();
-$("#catTo").hide();
-}
-else
-{
 $("#cat_select_items_button").show();
 $("#catTo").show();
 getDocs(space_url);
@@ -809,7 +802,6 @@ getDiscussions(space_url);
 getIdeas(space_url);
 getPolls(space_url);
 getBlogs(blog_url);
-}
 }
 function fromSpaceRequest() {
 // Handles user request to select the SPACE and then handle the response to fetch details about the selected SPACE.
