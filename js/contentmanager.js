f6fa8e3eb2d895f50121dfa6f74562cb5ad0b7ee
Ripul Thakur <ripul.thakur@accenture.com>
2013-04-30 15:40:10 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -51,6 +51,7 @@ function onPageLoad()
 {
 
 $('.test1').prop('checked', true);
+$('.testname').prop('checked', true);
 alert(document.getElementById("test1").value);
 // Detect the browser for adding different code when using IE.
 navigator.sayswho= (function(){    
